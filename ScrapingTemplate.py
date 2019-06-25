be78413723571e10e0da65cde4fa0f572f0608a2
Haniwasanndayo <33508020+Haniwasanndayo@users.noreply.github.com>
2019-06-25 15:48:39 +0900

    Create ScrapingTemplate.py

---
+++ ScrapingTemplate.py
@@ -0,0 +1,20 @@
+import urllib.request
+
+from bs4 import BeautifulSoup
+
+from selenium import webdriver
+
+
+# PhantomJSのドライバーを得る
+
+browser = webdriver.PhantomJS()
+browser.implicitly_wait(2)
+
+
+# Loginアクセス＆スープ
+
+url_login="hogehoge.com"
+
+browser.get(url_login)
+
+soup=bs4.BeautifulSoup(urllib.request.urlopen(url_login).read(), "html.parser")
